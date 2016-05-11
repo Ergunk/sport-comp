cb0c1d0198eb9bcef9c570c4b14cf2d198478c9f
ErgunK <erguen.kilic@rpn.ch>
2016-05-11 16:02:00 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+sport-comp
+==========
+
+A Symfony project created on May 11, 2016, 1:19 pm.
---
+++ var/cache/dev/twig/91/9125f62f65b51d496a3b272222e2f85be2773f46130df30cbd6a20bf78d3efb4.php
@@ -0,0 +1,40 @@
+<?php
+
+/* CoreBundle:Default:index.html.twig */
+class __TwigTemplate_eb8a03838fb2535a21eed9d17bb61682e8f34635f29d7561322a47944d1e0a67 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        $__internal_8dbd40b1d1ef9535bce499d14c7295e4f4c8a5e1acabcd50921be60f2b4e7f4e = $this->env->getExtension("native_profiler");
+        $__internal_8dbd40b1d1ef9535bce499d14c7295e4f4c8a5e1acabcd50921be60f2b4e7f4e->enter($__internal_8dbd40b1d1ef9535bce499d14c7295e4f4c8a5e1acabcd50921be60f2b4e7f4e_prof = new Twig_Profiler_Profile($this->getTemplateName(), "template", "CoreBundle:Default:index.html.twig"));
+
+        // line 1
+        echo "Hello World!
+";
+        
+        $__internal_8dbd40b1d1ef9535bce499d14c7295e4f4c8a5e1acabcd50921be60f2b4e7f4e->leave($__internal_8dbd40b1d1ef9535bce499d14c7295e4f4c8a5e1acabcd50921be60f2b4e7f4e_prof);
+
+    }
+
+    public function getTemplateName()
+    {
+        return "CoreBundle:Default:index.html.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  22 => 1,);
+    }
+}
+/* Hello World!*/
+/* */
